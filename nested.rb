ca3f652f4114a901901b945a47299fdfa16d00ac
Simon Kong <simon.kong95@gmail.com>
2018-12-19 07:52:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,8 +109,13 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-
+		 #programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 
+programmer_hash.each do |name, data|
+	if name == :alan_kay
+		
+	end	
+end
 
 end
 
